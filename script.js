2edad8328749be8a56c172f567520624bb627cda
Ayush Maheshwari <111050424+FrostScythe@users.noreply.github.com>
2025-09-25 21:25:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const bands = [
-      'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
-      'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
-      'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
-      'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
-    ];
+  'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
+  'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
+  'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
+  'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+];
 
-    function stripArticles(name) {
-      return name.replace(/^(a |an |the )/i, '').trim();
-    }
+function stripArticles(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
 
-    const sortedBands = bands.sort((a, b) => 
-      stripArticles(a).localeCompare(stripArticles(b))
-    );
+const sortedBands = bands.sort((a, b) =>
+  stripArticles(a).localeCompare(stripArticles(b))
+);
 
-    document.getElementById('band').innerHTML = 
-      sortedBands.map(band => `<li>${band}</li>`).join('');
+document.getElementById('bands').innerHTML =
+  sortedBands.map(band => `<li>${band}</li>`).join('');
